ff86ecfb683c2c605cfffa9881a32983adb81c42
Yaswant Narayan <yoyaswant@gmail.com>
2017-09-03 13:12:53 +0530

    git status

--- src/App.vue
+++ src/App.vue
@@ -16,20 +16,39 @@
     <v-flex xs12 sm8 offset-sm2>
       
       <br>
-<template>
 
-<template v-if="true">
-  <v-card hover @click="alertUser('yaswant')">
-    <v-card-text>
-      <p class="text-xs-center"><v-icon x-large>add</v-icon></p>
-      <p class="text-xs-center">Register new complaint</p>
-    </v-card-text>
-  </v-card>
-</template>
-      
+      <template v-if="isEdit">
+        <v-card hover @click="invertEdit">
+          <v-card-text>
+            <p class="text-xs-center"><v-icon x-large>add</v-icon></p>
+            <p class="text-xs-center">Register new complaint</p>
+          </v-card-text>
+        </v-card>
+        <br>
+      </template>
+
+    <template v-if="!isEdit">
+      <v-card>
+        <v-card-actions>
+            <v-radio-group v-model="type" :mandatory="false" row>
+              <v-spacer></v-spacer>
+              <v-radio label="Hostel" value="Hostel"></v-radio>
+              <v-radio label="Mess" value="Mess"></v-radio>
+              <v-spacer></v-spacer>
+            </v-radio-group>
+        </v-card-actions>
+        <p v-if="type=='Hostel'">You pressed hostel</p>
+        <p v-else>You pressed mess</P>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat class="orange--text">cancel</v-btn>
+          <v-btn @click="invertEdit" flat class="orange--text">submit</v-btn>
+        </v-card-actions>
+      </v-card>
       <br>
+    </template>
 
-    <template v-for="i in 3">
+    <template v-for="i in 2">
 
       <v-card>
         <v-card-title primary-title>
@@ -49,13 +68,13 @@
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
-      <v-stepper value="2">
+      <v-stepper value="3" class="hidden-sm-and-down">
         <v-stepper-header>
           <v-stepper-step step="1" complete>Logged
             <small>Summarize if needed</small>
           </v-stepper-step>
           <v-divider></v-divider>
-          <v-stepper-step step="2">Processing
+          <v-stepper-step step="2" complete>Processing
             <small>Summarize if needed</small>
           </v-stepper-step>
           <v-divider></v-divider>
@@ -65,25 +84,22 @@
         </v-stepper-header>
       </v-stepper>
 
-<template>
-  <v-stepper vertical>
-    <v-stepper-step step="1" v-bind:complete="e6 > 1">Select an app
-      <small>Summarize if needed</small>
-    </v-stepper-step>
-    <v-stepper-step step="2" v-bind:complete="e6 > 2">Configure analytics for this app
-      <small>Summarize if needed</small>
-    </v-stepper-step>
-    <v-stepper-step step="3" v-bind:complete="e6 > 3">Select an ad format and name ad unit
-      <small>Summarize if needed</small>
-    </v-stepper-step>
-  </v-stepper>
-</template>
+        <v-stepper value="2" vertical class="hidden-md-and-up">
+          <v-stepper-step step="1" complete>Logged
+            <small>Summarize if needed</small>
+          </v-stepper-step>
+          <v-stepper-step step="2">Processing
+            <small>Summarize if needed</small>
+          </v-stepper-step>
+          <v-stepper-step step="3">Completed
+            <small>Summarize if needed</small>
+          </v-stepper-step>
+        </v-stepper>
 
       <br>
 
     </template>
 
-    </template>
 
     </v-flex>
   </v-layout>
@@ -95,14 +111,17 @@
 
 <script>
 export default {
-  name: 'app',
-  data:{
-  },
-  methods:{
-    alertUser :function(message) {
-      alert(message);
+    data () {
+      return {
+        type: 'Hostel',
+        isEdit: true
+      }
+    },
+    methods: {
+      invertEdit () {
+        this.isEdit = !this.isEdit
+      }
     }
-  }
 }
 </script>
 

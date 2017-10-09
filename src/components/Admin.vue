ba5780eb2ff43ed5e9b38a87d4caf843a5dd7bc5
Yaswant Narayan <yoyaswant@gmail.com>
2017-10-09 11:44:58 +0530

    Before resolving sidenav issue

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <v-app id="example-3" toolbar>
+
             <v-navigation-drawer permanent floating light class="transparent hidden-sm-and-down">
                 <v-card class="ma-3">
                     <v-list dense class="py-0" :class="{myactive: selectedComponent=='dashboard-view'}">
@@ -25,6 +26,59 @@
                     </v-list>
                 </v-card>
             </v-navigation-drawer>
+
+            <!-- <v-card class="d-inline-block pa-5"> -->
+            <!-- <v-navigation-drawer permanent floating light>
+                <v-list dense class="pt-0">
+                    <v-list-tile @click="selectedComponent='dashboard-view'">
+                        <v-list-tile-action>
+                            <v-icon>dashboard</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Complaints</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    </v-list-tile>
+                    <v-list-tile @click="selectedComponent='departments-view'">
+                        <v-list-tile-action>
+                            <v-icon>people</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Departments</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-navigation-drawer> -->
+            <!-- </v-card> -->
+
+            <template>
+  <v-card class="d-inline-block pa-5">
+    <v-navigation-drawer permanent floating light>
+      <v-list dense class="pt-0">
+        <v-list-tile @click="selectedComponent='dashboard-view'">
+                            <v-list-tile-action>
+                                <v-icon>dashboard</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>Complaints</v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                        
+      </v-list>
+      <v-list dense class="pt-0">
+          <v-list-tile @click="selectedComponent='departments-view'">
+                            <v-list-tile-action>
+                                <v-icon>people</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>Departments</v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
             <v-toolbar class="primary" fixed dark>
                 <img src="../assets/logo.png" alt="logo" style="height:50px">
                 <v-toolbar-title>Admin console</v-toolbar-title>
@@ -39,10 +93,10 @@
                     <!-- <dashboard-view></dashboard-view> -->
                     <transition name="fade" mode="out-in">
                         <keep-alive>
-                        <component :is="selectedComponent">
-                            <!-- inactive components will be cached! -->
-                        </component>
-                    </keep-alive>
+                            <component :is="selectedComponent">
+                                <!-- inactive components will be cached! -->
+                            </component>
+                        </keep-alive>
                     </transition>
                 </v-container>
             </main>
@@ -69,21 +123,21 @@ export default {
 </script>
 
 <style scoped>
+.fade-enter-active {
+    transition: all 0.15s
+}
 
-    .fade-enter-active {
-        transition: all 0.15s
-    }
-    .fade-enter {
-        opacity: 0;
-    }
-    .fade-leave-active {
-        transition: all 0.15s;
-        opacity: 0;
-    }
+.fade-enter {
+    opacity: 0;
+}
 
-    myactive{
-        border-left-style: solid;
-        border-color: blue;
-    }
+.fade-leave-active {
+    transition: all 0.15s;
+    opacity: 0;
+}
 
+myactive {
+    border-left-style: solid;
+    border-color: blue;
+}
 </style>

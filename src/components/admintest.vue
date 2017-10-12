419b92de4ac4b2080bd310499676730ba0188575
Yaswant Narayan <yoyaswant@gmail.com>
2017-10-12 09:37:26 +0530

    CHange Admin Layout

--- src/components/admintest.vue
+++ src/components/admintest.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         <v-app>
+
             <v-navigation-drawer permanent light>
-                <v-toolbar flat>
+                <v-toolbar dark flat class="toolbar primary" style="background-color: #3B5998;">
+                    <v-spacer></v-spacer>
+                    <img src="../assets/logo.png" alt="logo" style="height:50px;margin-top:0px;">
+                    <v-spacer></v-spacer><br>
                     <v-list>
                         <v-list-tile>
-                            <v-list-tile-title class="title">
-                                Application
+                            <v-list-tile-title class="title" style="color: white;">
+                                Admin
                             </v-list-tile-title>
                         </v-list-tile>
                     </v-list>
@@ -24,10 +28,12 @@
                 </v-list>
             </v-navigation-drawer>
 
-            <main>
-                <v-container fluid>
-                    <!--v-router-->
-                    <!-- <dashboard-view></dashboard-view> -->
+            <!-- <main> -->
+            <v-container grid-list-md text-xs-center>
+                <!--v-router-->
+                <!-- <dashboard-view></dashboard-view> -->
+                <!-- <v-layout row wrap> -->
+                <v-flex xs10 offset-xs2>
                     <transition name="fade" mode="out-in">
                         <keep-alive>
                             <component :is="selectedComponent">
@@ -35,8 +41,26 @@
                             </component>
                         </keep-alive>
                     </transition>
-                </v-container>
-            </main>
+                </v-flex>
+                <!-- </v-layout> -->
+            </v-container>
+            <!-- </main> -->
+
+            <v-speed-dial v-model="fabConfig.fab" :top="fabConfig.top" :bottom="fabConfig.bottom" :right="fabConfig.right" :left="fabConfig.left" :direction="fabConfig.direction" :hover="fabConfig.hover" :transition="fabConfig.transition">
+                <v-btn slot="activator" color="blue darken-2" dark fab hover v-model="fab">
+                    <v-icon>account_circle</v-icon>
+                    <v-icon>close</v-icon>
+                </v-btn>
+                <v-btn fab dark small color="green">
+                    <v-icon>edit</v-icon>
+                </v-btn>
+                <v-btn fab dark small color="indigo">
+                    <v-icon>add</v-icon>
+                </v-btn>
+                <v-btn fab dark small color="red">
+                    <v-icon>delete</v-icon>
+                </v-btn>
+            </v-speed-dial>
         </v-app>
     </div>
 </template>
@@ -53,12 +77,61 @@ export default {
                 { title: 'About', icon: 'question_answer' }
             ],
             right: null,
-            selectedComponent: 'dashboard-view'
+            selectedComponent: 'dashboard-view',
+            fabConfig: {
+                direction: "top",
+                fab: false,
+                fling: false,
+                hover: false,
+                tabs: null,
+                top: false,
+                right: true,
+                bottom: true,
+                left: false,
+                transition: 'slide-y-reverse-transition'
+            }
         }
     },
     components: {
         'dashboard-view': Dashboard,
         'departments-view': Departments,
+    },
+
+    watch: {
+      top (val) {
+        this.fabConfig.bottom = !val
+      },
+      right (val) {
+        this.fabConfig.left = !val
+      },
+      bottom (val) {
+        this.fabConfig.top = !val
+      },
+      left (val) {
+        this.fabConfig.right = !val
+      }
+    },
+
+    computed: {
+      activeFab () {
+        switch (this.fabConfig.tabs) {
+          case 'one': return { 'class': 'purple', icon: 'account_circle' }
+          case 'two': return { 'class': 'red', icon: 'edit' }
+          case 'three': return { 'class': 'green', icon: 'keyboard_arrow_up' }
+          default: return {}
+        }
+      }
     }
 }
 </script>
+
+<style>
+  /* This is for documentation purposes and will not be needed in your application */
+  #create .speed-dial {
+    position: absolute;
+  }
+
+  #create .btn--floating {
+    position: relative;
+  }
+</style>

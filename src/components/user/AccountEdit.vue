aea190b6d86f61521130af46af4eab1b0278f19f
Yaswant Narayan <yoyaswant@gmail.com>
2017-10-20 23:59:15 +0530

    Added icon change

--- src/components/user/AccountEdit.vue
+++ src/components/user/AccountEdit.vue
@@ -54,10 +54,14 @@
           </v-list-tile>
           <v-list-tile avatar>
             <v-list-tile-action>
-              <v-btn flat icon color="primary"><v-icon>edit</v-icon></v-btn>
+              <v-btn flat icon color="primary" @click="editInfo.nameEdit = ! editInfo.nameEdit">
+                <v-icon v-if="editInfo.nameEdit">done</v-icon>
+                <v-icon v-else>edit</v-icon>
+              </v-btn>
             </v-list-tile-action>
             <v-list-tile-content>
               <v-list-tile-title>Name</v-list-tile-title>
+              <v-text-field v-if="editInfo.nameEdit" v-model="general.name" required></v-text-field>
               <v-list-tile-sub-title>{{ general.name }}</v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
@@ -92,8 +96,8 @@
                 </v-list-tile-action>
                 <v-list-tile-content>
                   <v-list-tile-title>Registration number</v-list-tile-title>
-                  <v-text-field v-if="editInfo.regnoEdit" v-model="general.regno" required></v-text-field>
-                  <v-list-tile-sub-title v-else>{{ general.regno }}</v-list-tile-sub-title>
+                  <v-text-field v-if="editInfo.nameEdit" v-model=" general.name " required></v-text-field>
+                  <v-list-tile-sub-title v-else>{{ general.name }}</v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-list-tile>
             </v-list>

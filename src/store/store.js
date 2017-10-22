4adcb510be4a5a139e0a7d6243c9de699febeb60
Yaswant Narayan <yoyaswant@gmail.com>
2017-10-22 19:17:14 +0530

    added some new variables

--- src/store/store.js
+++ src/store/store.js
@@ -5,21 +5,27 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        regno: '16BCE0904'
+        regno: '16BCE0904',
+        name: 'Yaswant Narayan',
+        mess: 'F-Block mess',
+        block: 'F-block',
+        floor: '5',
+        room: '654'
     },
     getters: {
         regno: state => {
             return state.regno;
+        },
+        name: store => {
+            return state.name;
+        },
+        mess: store => {
+            return state.mess;
         }
     },
     mutations: {
         updateregno: (state, payload) => {
             state.regno = payload;
-        }
-    },
-    actions: {
-        updateregno({commit},payload){
-            commit('updateregno', payload);
         }
     }
 });
